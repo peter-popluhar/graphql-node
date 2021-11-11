8cd017e44a8ac948b7a86c15a6081825498606cd
Peter Popluhár <peter.popluhar@heureka.cz>
2021-11-11 17:07:49 +0100

    realtime subscription

--- src/index.js
+++ src/index.js
@@ -2,18 +2,25 @@ const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 const fs = require("fs");
 const path = require("path");
+const { PubSub } = require("apollo-server");
+
+const Subscription = require('./resolvers/Subscription')
 const prisma = new PrismaClient();
+const pubsub = new PubSub()
 const { getUserId } = require("./utils");
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Vote = require('./resolvers/Vote')
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote
 };
 
 const server = new ApolloServer({
@@ -23,6 +30,7 @@ const server = new ApolloServer({
     return {
       ...req,
       prisma,
+      pubsub,
       userId: req && req.headers.authorization ? getUserId(req) : null,
     };
   },
--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,12 +1,28 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { APP_SECRET, getUserId } = require("../utils");
+const { APP_SECRET } = require("../utils");
+
+async function post(parent, args, context, info) {
+  const { userId } = context;
+
+  const newLink = await context.prisma.link.create({
+    data: {
+      url: args.url,
+      description: args.description,
+      postedBy: { connect: { id: userId } },
+    },
+  });
+  context.pubsub.publish("NEW_LINK", newLink);
+
+  return newLink;
+}
 
 async function signup(parent, args, context, info) {
   const password = await bcrypt.hash(args.password, 10);
   const user = await context.prisma.user.create({
     data: { ...args, password },
   });
+
   const token = jwt.sign({ userId: user.id }, APP_SECRET);
 
   return {
@@ -36,20 +52,35 @@ async function login(parent, args, context, info) {
   };
 }
 
-async function post(parent, args, context, info) {
+async function vote(parent, args, context, info) {
   const { userId } = context;
-
-  return await context.prisma.link.create({
-    data: {
-      url: args.url,
-      description: args.description,
-      postedBy: { connect: { id: userId } },
+  const vote = await context.prisma.vote.findUnique({
+    where: {
+      linkId_userId: {
+        linkId: Number(args.linkId),
+        userId: userId,
+      },
     },
   });
+
+  if (Boolean(vote)) {
+    throw new Error(`Already voted for link: ${args.linkId}`);
+  }
+
+  const newVote = context.prisma.vote.create({
+    data: {
+      user: { connect: { id: userId } },
+      link: { connect: { id: Number(args.linkId) } },
+    },
+  });
+  context.pubsub.publish("NEW_VOTE", newVote);
+
+  return newVote;
 }
 
 module.exports = {
+  post,
   signup,
   login,
-  post,
+  vote,
 };
